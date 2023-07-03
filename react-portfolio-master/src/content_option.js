0d01c9d41946a1c9867962de539d7ac0a2dc3be9
VinnyNepo <127804721+VinnyNepo@users.noreply.github.com>
2023-07-03 19:55:18 -0300

    push

---
+++ react-portfolio-master/src/content_option.js
@@ -0,0 +1,166 @@
+const logotext = "VINICIUS";
+const meta = {
+    title: "Vinicius Neponuceno",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "Vinicius Neponuceno",
+    animated: {
+        first: "Sou Estudante de ADS",
+        second: "Iniciando na area em 2023",
+        third: "eu amo programar",
+    },
+    description: "Estou em busca de realizar um sonhor, trabalhar na area da tecnologia",
+    your_img_url: "https://scontent.fops1-1.fna.fbcdn.net/v/t39.30808-6/357441329_1595604420964036_7045257885819841259_n.jpg?stp=dst-jpg_p526x296&_nc_cat=109&cb=99be929b-59f725be&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeHlBQZPNMTdA0ozO4c99nz_MzSYgkvsfp8zNJiCS-x-n10dFQDflx-Mwu0qpYqLqQlAZSbSW1zSDwP3wWoDoNwo&_nc_ohc=DtrX9qXWZhAAX8WAi32&_nc_ht=scontent.fops1-1.fna&oh=00_AfAIIuW0tQr-UB177Uq6n8D2l5hmF4DcZ81FkPk4GakyBw&oe=64A795F7",
+};
+
+const dataabout = {
+    title: "Um pouco sobre mim mesmo",
+    aboutme: " Estou iniciando minha carreira, pretendo me especializar na area da programação, buscar sempre dar meu maximo",
+};
+const worktimeline = [{
+        jobtitle: "Repositorio GitHub",
+        where: "Unifasipe",
+        date: "2023",
+    },
+    {
+        jobtitle: "Cybersecurity",
+        where: "Unifasipe",
+        date: "2023",
+    },
+    {
+        jobtitle: "Design Grafico",
+        where: "Jr Info",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 20,
+    },
+    {
+        name: "Html",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 20,
+    },
+    {
+        name: "React",
+        value: 30,
+    },
+    {
+        name: "CSS",
+        value: 60,
+    },
+];
+
+const services = [{
+        title: "",
+        description: "",
+    },
+    {
+        title: "",
+        description: "",
+    },
+    {
+        title: "",
+        description: "",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "Ainda não tenho projetos.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Ainda não tenho projetos.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Ainda não tenho projetos.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "Ainda não tenho projetos.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "Ainda não tenho projetos.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Ainda não tenho projetos.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "Ainda não tenho projetos.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Ainda não tenho projetos.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "Ainda não tenho projetos.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Ainda não tenho projetos.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "Ainda não tenho projetos.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "vinycius0neponuceno@gmail.com",
+    YOUR_FONE: "(99) 98470-0084",
+    description: "Entre em contato comigo ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "gmail",
+    YOUR_TEMPLATE_ID: "vinny",
+    YOUR_USER_ID: "pbJ9zGM37egr6e2fWHqc-",
+};
+
+const socialprofils = {
+    github: "https://github.com/VinnyNepo",
+    facebook: "https://www.facebook.com/vinicius.neponuceno.16",
+    linkedin: "https://www.linkedin.com/in/antonio-vinicius-lima-neponuceno-b3487022b/",
+    instagram: "https://www.instagram.com/vinycius_neponuceno/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
